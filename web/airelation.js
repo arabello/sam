077da8fe5116ca2553595144069c6ee7d69de4c1
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-02-05 21:14:50 +0100

    Add simple web front-end

---
+++ web/airelation.js
@@ -0,0 +1,37 @@
+$(function() {
+    var ctx = document.getElementById("chart").getContext('2d');
+
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:8080",
+        dataType: "json",
+        success: function (data) {
+            createChart(data);
+        }
+    });
+
+    function createChart(jsonData){
+        var chart = new Chart(ctx, {
+            type: 'bubble',
+            data: {
+                datasets: jsonData.datasets
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            max: 1
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            max: 1
+                        }
+                    }]
+                }
+            }
+        });
+    }
+});
